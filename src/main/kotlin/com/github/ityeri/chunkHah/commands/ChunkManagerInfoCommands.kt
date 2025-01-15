0149361822907b9bd558e1c8af47fae84cc3dece
ityeri <yeongbin0909@naver.com>
2025-01-15 09:03:28 +0900

    명령어 사용후 나오는 메세지 가독성 수정

--- src/main/kotlin/com/github/ityeri/chunkHah/commands/ChunkManagerInfoCommands.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/commands/ChunkManagerInfoCommands.kt
@@ -22,7 +22,7 @@ class ChunkManagerInfoCommands(val plugin: JavaPlugin, val chunkHandler: ChunkHa
     class AllChunkManagerCommand(val chunkHandler: ChunkHandler) : BaseCommand() {
         @Default
         fun onCommand(sender: Player) {
-            val chunkManagerSet = chunkHandler.chunkManagerSet
+            val chunkManagerSet = chunkHandler.getChunkManagers()
 
             if (chunkManagerSet.size == 0) {
                 sender.sendMessage("청그 매니저가 없습니다")
@@ -30,11 +30,12 @@ class ChunkManagerInfoCommands(val plugin: JavaPlugin, val chunkHandler: ChunkHa
                 sender.sendMessage("현재 ${chunkManagerSet.size}개의 청크 매니저가 있습니다")
 
                 for (chunkManager in chunkManagerSet) {
-                    sender.sendMessage(
-                        """
-                        청크 위치: ${sender.chunk.x}, ${sender.chunk.z}
-                        플레이어: ${chunkManager.playerName ?: "알수없음 (플레이어 이름은 플레이어가 한번이라도 서버에 접속했을때만 확인이 가능합니다)"}
-                        플레이어 UUID: ${chunkManager.playerUUID}""".trimIndent()
+                    sender.sendMessage("""
+                        --------------------------------------------------
+                        | 
+                        | 청크 위치: ${sender.chunk.x}, ${sender.chunk.z}
+                        | 플레이어: ${chunkManager.playerName ?: "알수없음 (플레이어 이름은 플레이어가 한번이라도 서버에 접속했을때만 확인이 가능합니다)"}
+                        | 플레이어 UUID: ${chunkManager.playerUUID}""".trimIndent()
                     )
                 }
             }
@@ -54,11 +55,12 @@ class ChunkManagerInfoCommands(val plugin: JavaPlugin, val chunkHandler: ChunkHa
                 sender.sendMessage("해당 청크에는 ${chunkManagerSet.size}명의 플레이어가 있습니다")
 
                 for (chunkManager in chunkManagerSet) {
-                    sender.sendMessage(
-                        """
-                청크 위치: ${sender.chunk.x}, ${sender.chunk.z}
-                플레이어: ${chunkManager.playerName ?: "알수없음 (플레이어 이름은 플레이어가 한번이라도 서버에 접속했을때만 확인이 가능합니다)"}
-                플레이어 UUID: ${chunkManager.playerUUID}""".trimIndent()
+                    sender.sendMessage("""
+                        --------------------------------------------------
+                        | 
+                        | 청크 위치: ${sender.chunk.x}, ${sender.chunk.z}
+                        | 플레이어: ${chunkManager.playerName ?: "알수없음 (플레이어 이름은 플레이어가 한번이라도 서버에 접속했을때만 확인이 가능합니다)"}
+                        | 플레이어 UUID: ${chunkManager.playerUUID}""".trimIndent()
                     )
                 }
             }
