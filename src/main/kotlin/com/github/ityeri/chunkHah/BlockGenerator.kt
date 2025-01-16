16f2e93257280a122d156ed3e82d1ad4c2b72cba
ityeri <yeongbin0909@naver.com>
2025-01-17 08:46:32 +0900

    BlockGenerator 로 이름 변경

---
+++ src/main/kotlin/com/github/ityeri/chunkHah/BlockGenerator.kt
@@ -0,0 +1,11 @@
+package com.github.ityeri.chunkHah
+
+import org.bukkit.block.Block
+
+class BlockGenerator(
+    val centerY: Int,
+    val range: Int,
+    val generatingBlock: Block
+) {
+
+}
--- src/main/kotlin/com/github/ityeri/chunkHah/MineralGenerator.kt
+++
@@ -1,4 +0,0 @@
-package com.github.ityeri.chunkHah
-
-class MineralGenerator {
-}
