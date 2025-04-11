66f904f4366a157b7bfb36c24b4f80f127ca6e78
it <yeongbin0909@naver.com>
2025-04-12 02:19:20 +0900

    area get, add 관련 매서드 추가

--- src/main/kotlin/com/github/ityeri/chunkHah/core/PlayerAreaManager.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/core/PlayerAreaManager.kt
@@ -1,8 +1,16 @@
 package com.github.ityeri.chunkHah.core
 
+import org.bukkit.OfflinePlayer
+import org.bukkit.entity.Player
 import java.util.UUID
 
 class PlayerAreaManager {
 
     private val playerAreaMap: MutableMap<UUID, Area> = mutableMapOf()
+
+    fun getAllArea(): Map<UUID, Area> = playerAreaMap
+
+    fun addArea(player: Player, area: Area) { playerAreaMap[player.uniqueId] = area }
+    fun getArea(player: OfflinePlayer): Area? = playerAreaMap[player.uniqueId]
+
 }
