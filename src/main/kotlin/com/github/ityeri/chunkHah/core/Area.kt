1d75256aa8691dc61e453b7af8f9925e57260491
it <yeongbin0909@naver.com>
2025-04-12 02:36:16 +0900

    기본 생성자 추가

--- src/main/kotlin/com/github/ityeri/chunkHah/core/Area.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/core/Area.kt
@@ -1,4 +1,16 @@
 package com.github.ityeri.chunkHah.core
 
-class Area {
+import org.bukkit.entity.Player
+import java.util.UUID
+
+class Area(
+    val playerUUID: UUID, val areaManager: AreaManager,
+    var x: Int, var z: Int,
+) {
+
+    constructor(
+        player: Player, areaManager: AreaManager,
+        x: Int, z: Int,
+        width: Int, depth: Int) :
+            this(player.uniqueId, areaManager, x, z)
 }
