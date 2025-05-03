6f060d5e641c8b8337d3f6efc1265178bcca2127
it <yeongbin0909@naver.com>
2025-05-03 15:59:14 +0900

    소유 여부, 플레이어 관련 속성(게터) 추가

--- src/main/kotlin/com/github/ityeri/chunkHah/core/Area.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/core/Area.kt
@@ -1,16 +1,30 @@
 package com.github.ityeri.chunkHah.core
 
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import java.util.UUID
 
 class Area(
-    val playerUUID: UUID, val areaManager: AreaManager,
+    var playerUUID: UUID?, val areaManager: AreaManager,
     var x: Int, var z: Int,
 ) {
 
     constructor(
-        player: Player, areaManager: AreaManager,
-        x: Int, z: Int,
-        width: Int, depth: Int) :
-            this(player.uniqueId, areaManager, x, z)
+        player: Player?, areaManager: AreaManager,
+        x: Int, z: Int):
+            this(player?.uniqueId, areaManager, x, z)
+
+    val isOwned: Boolean get() = playerUUID != null
+
+    val isPlayerOnline: Boolean?
+        get() {
+            playerUUID?.let { return Bukkit.getPlayer(it) != null }
+            return null
+        }
+
+    val player: Player?
+        get() {
+            playerUUID?.let { return Bukkit.getPlayer(playerUUID!!) }
+            return null
+        }
 }
