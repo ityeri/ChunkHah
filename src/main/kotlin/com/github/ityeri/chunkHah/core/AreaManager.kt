54e75b29fb5d12da80395485da58ddee1e36273f
it <yeongbin0909@naver.com>
2025-05-24 11:03:27 +0900

    disable 에 이벤트 핸들러 제거, 태스크 제거 코드 추가

--- src/main/kotlin/com/github/ityeri/chunkHah/core/AreaManager.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/core/AreaManager.kt
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit
 import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
+import org.bukkit.event.HandlerList
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
@@ -15,17 +16,21 @@ class AreaManager(
 ) : Listener {
 
     private val playerAreaMap: MutableMap<UUID, Area> = mutableMapOf()
+    private var updateTaskId: Int? = null
 
     fun enable() {
         Bukkit.getPluginManager().registerEvents(this, plugin)
 
-        Bukkit.getScheduler().runTaskTimer(plugin, Runnable {
+        updateTaskId = Bukkit.getScheduler().runTaskTimer(plugin, Runnable {
             playerAreaMap.forEach { it.value.update() }
-        }, 0L, 1L)
+        }, 0L, 1L).taskId
     }
 
     fun disable() {
 
+
+        HandlerList.unregisterAll(this)
+        Bukkit.getScheduler().cancelTask(updateTaskId!!)
     }
 
     fun getAllArea(): Map<UUID, Area> = playerAreaMap
