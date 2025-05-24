8c4dbb028803564917b9a1f04dc2a723ba5469fe
it <yeongbin0909@naver.com>
2025-05-24 10:48:43 +0900

    PlayerJoin 이벤트 핸틀러 추가 (미구현)

--- src/main/kotlin/com/github/ityeri/chunkHah/core/AreaManager.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/core/AreaManager.kt
@@ -2,13 +2,16 @@ package com.github.ityeri.chunkHah.core
 
 import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
 import java.util.UUID
 
 class AreaManager(
     val plugin: JavaPlugin,
     var areaWidth: Int, var areaDepth: Int
-) {
+) : Listener {
 
     private val playerAreaMap: MutableMap<UUID, Area> = mutableMapOf()
 
@@ -24,6 +27,13 @@ class AreaManager(
 
     fun addArea(player: Player, area: Area) { playerAreaMap[player.uniqueId] = area }
     fun getArea(player: OfflinePlayer): Area? = playerAreaMap[player.uniqueId]
+
+
+
+    @EventHandler
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+
+    }
 
 }
 
