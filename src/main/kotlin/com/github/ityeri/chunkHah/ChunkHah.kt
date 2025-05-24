8d2066444357da22be171dd03ba2385782d6de96
it <yeongbin0909@naver.com>
2025-05-24 16:36:23 +0900

    AreaManager 생성자에 플러그인 객체도 전달하도록 변경

--- src/main/kotlin/com/github/ityeri/chunkHah/ChunkHah.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/ChunkHah.kt
@@ -7,7 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class ChunkHah : JavaPlugin() {
 
-    val areaManager = AreaManager(8, 8)
+    val areaManager = AreaManager(this, 8, 8)
 
     override fun onEnable() {
         Bukkit.getLogger().info("청크핳")
