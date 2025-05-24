31d47be2c0f80ca3d0f53eba205b9cb3ab01ba9b
it <yeongbin0909@naver.com>
2025-05-24 10:47:23 +0900

    생성자 인자에 plugin 인자 추가

--- src/main/kotlin/com/github/ityeri/chunkHah/core/AreaManager.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/core/AreaManager.kt
@@ -2,9 +2,11 @@ package com.github.ityeri.chunkHah.core
 
 import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
+import org.bukkit.plugin.java.JavaPlugin
 import java.util.UUID
 
 class AreaManager(
+    val plugin: JavaPlugin,
     var areaWidth: Int, var areaDepth: Int
 ) {
 
