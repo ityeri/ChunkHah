2453678984dee19ddcfaac3387229792b9069430
it <yeongbin0909@naver.com>
2025-05-24 10:34:08 +0900

    새로 작성된 AreaManager 를 사용하도록 변경

--- src/main/kotlin/com/github/ityeri/chunkHah/ChunkHah.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/ChunkHah.kt
@@ -1,46 +1,19 @@
 package com.github.ityeri.chunkHah
 
-import com.github.ityeri.chunkHah.commands.AriaControlCommand
-import com.github.ityeri.chunkHah.commands.BindingCommands
-import com.github.ityeri.chunkHah.commands.ChunkManagerInfoCommands
+import com.github.ityeri.chunkHah.core.AreaManager
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
-import java.io.FileNotFoundException
-import com.github.ityeri.chunkHah.utils.HitboxUtils
 
-// TODO 리소스 테스크
 
 class ChunkHah : JavaPlugin() {
-    private val hitboxUtils: HitboxUtils = HitboxUtils()
-    val chunkHandler = ChunkHandler(this)
 
-
+    val areaManager = AreaManager(8, 8)
 
     override fun onEnable() {
-
-        try {
-            chunkHandler.loadAriaDatas()
-        } catch (e: FileNotFoundException) {
-            Bukkit.getLogger().warning("영역 데이터 파일을 찾을수 없습니다. 자동 할당을 사용합니다")
-        } catch (e: ChunkHandler.WrongAriaDataException) {
-            Bukkit.getLogger().warning("영역 데이터 파일의 구조가 잘못됬습니다. 자동 할당을 사용합니다")
-        }
-
-        // 명령어 등록
-        BindingCommands(this, chunkHandler).onEnable()
-        ChunkManagerInfoCommands(this, chunkHandler).onEnable()
-        AriaControlCommand(this, chunkHandler).onEnable()
-
-        // 엔더맨 거시시
-        EndPortalFrameDropper(this).onEnable()
-
-        chunkHandler.onEnable()
-
-        Bukkit.getLogger().info("이것은 당신의 청크하가 매우 정상적으로 켜졌다는 의미일까요?")
+        Bukkit.getLogger().info("청크핳")
     }
 
     override fun onDisable() {
-        chunkHandler.saveAriaData()
 
     }
 }
