9607db33ee1639231be90a8b762cd7c6f4efe067
it <yeongbin0909@naver.com>
2025-05-27 07:24:10 +0900

    SerAreaCommand 전체 기능 구현 완료

--- src/main/kotlin/com/github/ityeri/chunkHah/userinterface/SetAreaCommand.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/userinterface/SetAreaCommand.kt
@@ -5,20 +5,13 @@ import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandCompletion
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Default
+import com.github.ityeri.chunkHah.core.Area
 import com.github.ityeri.chunkHah.core.AreaManager
 import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
-/*
-@CommandAlias("setchunk")
-@CommandPermission("op")
-class SetChunkCommand(val chunkHandler: ChunkHandler) : BaseCommand() {
-    @Default
-    @CommandCompletion("@players @currentChunkX @currentChunkZ")
-    fun onCommand(sender: CommandSender, targetPlayerName: String, newChunkXstr: String, newChunkZstr: String) {
 
- */
 
 @CommandAlias("setarea")
 @CommandPermission("op")
@@ -31,6 +24,21 @@ class SetAreaCommand(val areaManager: AreaManager) : BaseCommand() {
             return
         }
 
+        val areaX = (sender.x / areaManager.areaWidth).toInt()
+        val areaZ = (sender.z / areaManager.areaDepth).toInt()
+
         val targetPlayer = Bukkit.getPlayer(targetPlayerName)!!
+
+        var area = areaManager.getArea(targetPlayer)
+
+        area ?: run {
+            area = Area(targetPlayer, areaManager, 0, 0)
+            areaManager.addArea(area!!)
+        }
+
+        area!!.x = areaX
+        area!!.z = areaZ
+
+        sender.sendMessage("플레이어 \"$targetPlayerName\" 의 영역을 [$areaX, $areaZ] 로 배치했습니다")
     }
 }
