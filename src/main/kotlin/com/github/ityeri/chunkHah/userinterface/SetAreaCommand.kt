26ec60d4a3424ff392a1b192c7469010f6494ad8
it <yeongbin0909@naver.com>
2025-05-26 14:48:19 +0900

    SetAreaCommand 클래스 추가

---
+++ src/main/kotlin/com/github/ityeri/chunkHah/userinterface/SetAreaCommand.kt
@@ -0,0 +1,7 @@
+package com.github.ityeri.chunkHah.userinterface
+
+import co.aikar.commands.BaseCommand
+import com.github.ityeri.chunkHah.core.AreaManager
+
+class SetAreaCommand(val areaManager: AreaManager) : BaseCommand() {
+}
