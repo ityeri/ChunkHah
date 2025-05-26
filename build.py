f39f868f21d44d66308220b80d4e469b968945b5
it <yeongbin0909@naver.com>
2025-05-27 08:36:34 +0900

    메타데이터 파일 제거 기능 추가

--- build.py
+++ build.py
@@ -19,26 +19,45 @@ import subprocess
 import _config
 import paramiko
 import time
+import os
+
+def dot_clean(directory):
+    # directory 안의 파일과 폴더를 재귀적으로 탐색
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            # 파일이 '._'로 시작하는지 확인
+            if file.startswith("._"):
+                file_path = os.path.join(root, file)
+                try:
+                    os.remove(file_path)
+                    print(f"파일 삭제됨: {file_path}")
+                except Exception as e:
+                    print(f"파일 삭제 실패: {file_path} - {e}")
 
 while True:
 
-    print("빌드중...")
+    print("메타데이터 파일 제거...")
+    dot_clean(os.getcwd())
+    print("메타데이터 파일 제거 완료")
 
+    print("빌드중...")
     subprocess.run(["./gradlew", "jar"])
 
     print("\nSSH 접속...")
-
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     client.connect(_config.SSH_ADDRESS, username=_config.SSH_USERNAME, password=_config.SSH_PASSWORD)
 
     print("빌드 파일 전송...")
-
     sftp = client.open_sftp()
     sftp.put(_config.COPY_PATH, _config.PASTE_PATH)
-
     print("빌드 파일 전송 완료")
+
+    print("메타데이터 파일 제거...")
+    dot_clean(os.getcwd())
+    print("메타데이터 파일 제거 완료")
+
 
     break
 
