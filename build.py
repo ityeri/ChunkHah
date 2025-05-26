5424ba2560710348e7e5cf63cbbbe7d28a695cc7
it <yeongbin0909@naver.com>
2025-05-27 08:14:57 +0900

    자동 리로드 기능 비활성화

--- build.py
+++ build.py
@@ -36,12 +36,12 @@ print("빌드 파일 전송...")
 sftp = client.open_sftp()
 sftp.put(_config.COPY_PATH, _config.PASTE_PATH)
 
-print("서버 명령 전송...")
-
-channel = client.invoke_shell()
-
-channel.send(f"screen -d -r {_config.SCREEN_NAME} \n")
-time.sleep(1)
-channel.send("reload confirm \n")
-
-print("리로드 전송을 완료했습니다!")
+# print("서버 명령 전송...")
+#
+# channel = client.invoke_shell()
+#
+# channel.send(f"screen -d -r {_config.SCREEN_NAME} \n")
+# time.sleep(1)
+# channel.send("reload confirm \n")
+#
+# print("리로드 전송을 완료했습니다!")
