dbd666cebf534c3cedc0d75228526fe7accd314f
it <yeongbin0909@naver.com>
2025-05-26 14:55:09 +0900

    명령어 클래스 골조 추가

--- src/main/kotlin/com/github/ityeri/chunkHah/userinterface/SetAreaCommand.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/userinterface/SetAreaCommand.kt
@@ -1,7 +1,30 @@
 package com.github.ityeri.chunkHah.userinterface
 
 import co.aikar.commands.BaseCommand
+import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.CommandCompletion
+import co.aikar.commands.annotation.CommandPermission
+import co.aikar.commands.annotation.Default
 import com.github.ityeri.chunkHah.core.AreaManager
+import org.bukkit.Bukkit
+import org.bukkit.command.CommandSender
 
+/*
+@CommandAlias("setchunk")
+@CommandPermission("op")
+class SetChunkCommand(val chunkHandler: ChunkHandler) : BaseCommand() {
+    @Default
+    @CommandCompletion("@players @currentChunkX @currentChunkZ")
+    fun onCommand(sender: CommandSender, targetPlayerName: String, newChunkXstr: String, newChunkZstr: String) {
+
+ */
+
+@CommandAlias("setarea")
+@CommandPermission("op")
 class SetAreaCommand(val areaManager: AreaManager) : BaseCommand() {
+    @Default
+    @CommandCompletion("@players")
+    fun onCommand(sender: CommandSender, targetPlayerName: String) {
+        val targetPlayer = Bukkit.getPlayer(targetPlayerName)!!
+    }
 }
