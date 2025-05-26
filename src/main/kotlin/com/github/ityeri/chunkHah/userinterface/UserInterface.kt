d9a2e9e3531c3cbeb382863caddad55d0e4be2c9
it <yeongbin0909@naver.com>
2025-05-27 07:31:48 +0900

    SetAreaCommand 등록 추가

--- src/main/kotlin/com/github/ityeri/chunkHah/userinterface/UserInterface.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/userinterface/UserInterface.kt
@@ -1,9 +1,13 @@
 package com.github.ityeri.chunkHah.userinterface
 
+import co.aikar.commands.PaperCommandManager
 import com.github.ityeri.chunkHah.core.AreaManager
 
-class UserInterface(areaManager: AreaManager) {
+class UserInterface(val areaManager: AreaManager) {
     fun enable() {
+        val manager = PaperCommandManager(areaManager.plugin)
+
+        manager.registerCommand(SetAreaCommand(areaManager))
 
     }
 }
