6c50fba93034f5cef2404046869866d0ba3f71df
it <yeongbin0909@naver.com>
2025-05-27 09:38:12 +0900

    serializer 패키지로 이동

--- src/main/kotlin/com/github/ityeri/chunkHah/core/AreaLoader.kt
+++
@@ -1,4 +0,0 @@
-package com.github.ityeri.chunkHah.core
-
-class AreaLoader(val areaManager: AreaManager) {
-}
---
+++ src/main/kotlin/com/github/ityeri/chunkHah/core/serializer/AreaLoader.kt
@@ -0,0 +1,6 @@
+package com.github.ityeri.chunkHah.core.serializer
+
+import com.github.ityeri.chunkHah.core.AreaManager
+
+class AreaLoader(val areaManager: AreaManager) {
+}
