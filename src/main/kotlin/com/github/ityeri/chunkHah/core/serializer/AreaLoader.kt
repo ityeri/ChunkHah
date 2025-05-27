e1814ea727b6a118704a09e6c27f717bc4fe2a30
it <yeongbin0909@naver.com>
2025-05-27 10:23:45 +0900

    AreaLoader 저장 구현

--- src/main/kotlin/com/github/ityeri/chunkHah/core/serializer/AreaLoader.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/core/serializer/AreaLoader.kt
@@ -1,6 +1,30 @@
 package com.github.ityeri.chunkHah.core.serializer
 
 import com.github.ityeri.chunkHah.core.AreaManager
+import kotlinx.serialization.json.*
+import java.io.File
 
 class AreaLoader(val areaManager: AreaManager) {
+    fun save() {
+
+        val json = Json {
+            prettyPrint = true
+            prettyPrintIndent = "    "
+        }
+
+        val serializedAreas = buildJsonArray {
+            for (area in areaManager.getAllArea()) {
+                add(json.encodeToString(AreaSerializer, area))
+            }
+        }
+
+        val data = buildJsonObject {
+            put("areaWidth", areaManager.areaWidth)
+            put("areaDepth", areaManager.areaDepth)
+            put("areas", serializedAreas)
+        }
+
+        val file = File(areaManager.plugin.dataFolder, "area_data.json")
+        file.writeText(json.encodeToString(JsonObject.serializer(), data))
+    }
 }
