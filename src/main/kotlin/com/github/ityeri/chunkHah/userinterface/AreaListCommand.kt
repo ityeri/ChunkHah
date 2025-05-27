2249b783511df576f4946749996e5a8a8052c132
it <yeongbin0909@naver.com>
2025-05-27 14:36:30 +0900

    영역 정보 가독성 수정

--- src/main/kotlin/com/github/ityeri/chunkHah/userinterface/AreaListCommand.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/userinterface/AreaListCommand.kt
@@ -5,28 +5,24 @@ import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Default
 import com.github.ityeri.chunkHah.core.AreaManager
-import com.github.ityeri.chunkHah.core.serializer.AreaSerializer
-import kotlinx.serialization.json.Json
+import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
 
 @CommandAlias("areas")
 @CommandPermission("op")
 class AreaListCommand(val areaManager: AreaManager) : BaseCommand() {
 
-    val json = Json {
-        prettyPrint = true
-        prettyPrintIndent = "    "
-    }
-
     @Default
     fun onCommand(sender: CommandSender) {
         val areas = areaManager.getAllArea()
 
-        sender.sendMessage("총 $areas 개의 영역이 있습니다")
+        sender.sendMessage("총 ${areas.size}개의 영역이 있습니다")
 
         for (area in areas) {
-            sender.sendMessage("\n===")
-            sender.sendMessage(json.encodeToString(AreaSerializer, area))
+            sender.sendMessage(" |  ")
+            sender.sendMessage(" |  플레이어: ${Bukkit.getOfflinePlayer(area.playerUUID).name}")
+            sender.sendMessage(" |  위치: [${area.x}, ${area.z}]")
+            sender.sendMessage(" |  제약 활성화 여부: ${area.enabled}")
         }
     }
 }
