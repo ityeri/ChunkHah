fa40b4abbaff0caea41d81e141b15bd1ebd060a5
it <yeongbin0909@naver.com>
2025-05-27 13:18:48 +0900

    영역 파일 로드 기능 추가

--- src/main/kotlin/com/github/ityeri/chunkHah/core/serializer/AreaLoader.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/core/serializer/AreaLoader.kt
@@ -3,6 +3,7 @@ package com.github.ityeri.chunkHah.core.serializer
 import com.github.ityeri.chunkHah.core.AreaManager
 import kotlinx.serialization.json.*
 import java.io.File
+import java.io.FileNotFoundException
 
 class AreaLoader(val areaManager: AreaManager) {
     fun save() {
@@ -33,5 +34,23 @@ class AreaLoader(val areaManager: AreaManager) {
         }
 
         file.writeText(json.encodeToString(JsonObject.serializer(), data))
+    }
+
+    fun load() {
+        val file = File(areaManager.plugin.dataFolder, "area_data.json")
+        if (!file.exists()) {
+            throw FileNotFoundException("영역파일 area_data.json 이 존재하지 않습니다")
+        }
+
+        val data = Json.parseToJsonElement(file.readText()) as JsonObject
+
+        areaManager.areaWidth = data["width"]!!.jsonPrimitive.int
+        areaManager.areaDepth = data["depth"]!!.jsonPrimitive.int
+
+        areaManager.removeAllArea()
+
+        data["areas"]!!.jsonPrimitive.jsonArray.forEach {
+            areaManager.addArea(Json.decodeFromJsonElement(AreaSerializer, it))
+        }
     }
 }
