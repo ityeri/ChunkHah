55614326b3a227cc1bef240ee2e280cd960e5cca
it <yeongbin0909@naver.com>
2025-05-27 15:58:21 +0900

    RunCommand 기본 기능 구현

--- src/main/kotlin/com/github/ityeri/chunkHah/userinterface/RunCommand.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/userinterface/RunCommand.kt
@@ -6,13 +6,22 @@ import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Default
 import com.github.ityeri.chunkHah.core.AreaManager
 import org.bukkit.command.CommandSender
+import javax.script.ScriptEngineManager
 
 @CommandAlias("run")
 @CommandPermission("op")
 class RunCommand(val areaManager: AreaManager) : BaseCommand() {
 
+    val engine = ScriptEngineManager().getEngineByName("JavaScript")
+
+    init {
+        engine.put("manager", areaManager)
+    }
+
     @Default
     fun onCommand(sender: CommandSender, code: String) {
 
+        val result = engine.eval(code)
+        sender.sendMessage(result.toString())
     }
 }
