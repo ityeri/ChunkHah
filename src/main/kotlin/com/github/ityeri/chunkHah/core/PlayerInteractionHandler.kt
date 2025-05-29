174b4aa03aac966d382a81b10b2522c4c15f2a99
it <yeongbin0909@naver.com>
2025-05-29 23:42:51 +0900

    플레이어 우클릭 방지 코드 추가

--- src/main/kotlin/com/github/ityeri/chunkHah/core/PlayerInteractionHandler.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/core/PlayerInteractionHandler.kt
@@ -4,6 +4,20 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerInteractEvent
 
-class PlayerInteractionHandler(area: Area) : Listener {
+class PlayerInteractionHandler(val area: Area) : Listener {
 
+    @EventHandler
+    fun onPlayerInteractHandler(event: PlayerInteractEvent) {
+
+        if (event.player != area.player) { return }
+        if (event.clickedBlock == null) { return }
+
+        val block = event.clickedBlock!!
+
+        if (!(area.minX <= block.x && block.x < area.maxX &&
+                    area.minZ <= block.z && block.z < area.maxZ)) {
+
+            event.isCancelled = true
+        }
+    }
 }
