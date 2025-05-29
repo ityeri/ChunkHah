f5eeff2ecae37cc4df6e3cad84754d4b90ecf7b2
it <yeongbin0909@naver.com>
2025-05-29 15:59:52 +0900

    월드 이동관련 처리 코드를 WorldChangeHandler 로 이동

--- src/main/kotlin/com/github/ityeri/chunkHah/core/WorldChangeHandler.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/core/WorldChangeHandler.kt
@@ -1,4 +1,107 @@
 package com.github.ityeri.chunkHah.core
 
-class WorldChangeHandler(val area: Area) {
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.World
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerChangedWorldEvent
+import org.bukkit.event.world.PortalCreateEvent
+import org.bukkit.event.world.PortalCreateEvent.CreateReason
+import kotlin.random.Random
+
+class WorldChangeHandler(val area: Area) : Listener {
+
+    @EventHandler
+    fun onPlayerChangeWorld(event: PlayerChangedWorldEvent) {
+
+        if (event.player != area.player) { return }
+
+        val fromWorld = event.from
+        val toWorld = area.player!!.world
+
+        // 오버 -> 네더
+        if (fromWorld.environment == World.Environment.NORMAL &&
+            toWorld.environment == World.Environment.NETHER) {
+
+            while (true) {
+                val spawnBlockX = Random.nextInt(area.minX, area.maxX)
+                val spawnBlockY = Random.nextInt(1, 120)
+                val spawnBlockZ = Random.nextInt(area.minZ, area.maxZ)
+
+                val groundBlock = toWorld.getBlockAt(
+                    spawnBlockX,
+                    spawnBlockY - 1,
+                    spawnBlockZ
+                )
+                val lowerBlock = toWorld.getBlockAt(
+                    spawnBlockX,
+                    spawnBlockY,
+                    spawnBlockZ
+                )
+                val upperBlock = toWorld.getBlockAt(
+                    spawnBlockX,
+                    spawnBlockY + 1,
+                    spawnBlockZ
+                )
+
+                if (groundBlock.isSolid &&
+                    lowerBlock.isEmpty &&
+                    upperBlock.isEmpty) {
+
+                    area.player!!.teleport(
+                        Location(
+                            toWorld,
+                            spawnBlockX + 0.5,
+                            spawnBlockY + 0.5,
+                            spawnBlockZ + 0.5
+                        )
+                    )
+
+                    break
+                }
+            }
+
+            // TODO 빈공간이 없어서 무한 루프에 갇힐경우 로직 추가
+
+        }
+
+        // 네더 -> 오버
+        else if (fromWorld.environment == World.Environment.NETHER &&
+            toWorld.environment == World.Environment.NORMAL) {
+
+            while (true) {
+
+                val spawnBlockX = Random.nextInt(area.minX, area.maxX)
+                val spawnBlockZ = Random.nextInt(area.minZ, area.maxZ)
+
+                val groundBlock = toWorld.getHighestBlockAt(spawnBlockX, spawnBlockZ)
+                val spawnBlockY = groundBlock.y + 1
+
+                if (groundBlock.isSolid) {
+                    Bukkit.getScheduler().runTaskLater(area.areaManager!!.plugin, Runnable {
+                        area.player!!.teleport(
+                            Location(
+                            toWorld,
+                            spawnBlockX + 0.5,
+                            spawnBlockY + 0.5,
+                            spawnBlockZ + 0.5
+                        )
+                        )
+                    }, 1L)
+
+                    break
+                }
+
+            }
+
+        }
+    }
+
+    @EventHandler
+    fun onPortalCreate(event: PortalCreateEvent) {
+        if (event.reason == CreateReason.NETHER_PAIR) {
+            event.isCancelled = true
+        }
+    }
 }
