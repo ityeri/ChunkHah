5739d96471a6831369064a6b61b4f3ee8c9e75d6
it <yeongbin0909@naver.com>
2025-05-29 23:16:24 +0900

    PlayerInteractionHandler 코드 골조 추가

---
+++ src/main/kotlin/com/github/ityeri/chunkHah/core/PlayerInteractionHandler.kt
@@ -0,0 +1,9 @@
+package com.github.ityeri.chunkHah.core
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerInteractEvent
+
+class PlayerInteractionHandler(area: Area) : Listener {
+
+}
