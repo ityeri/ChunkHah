172797cbb3e6c770ec827c55c523559d31fe4ed5
it <yeongbin0909@naver.com>
2025-05-31 21:52:20 +0900

    FirstWorldEntryHandler 코드 골조 추가

--- src/main/kotlin/com/github/ityeri/chunkHah/core/FirstWorldEntryHandler.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/core/FirstWorldEntryHandler.kt
@@ -1,7 +1,31 @@
 package com.github.ityeri.chunkHah.core
 
-class FirstWorldEntryHandler(area: Area) {
-    fun update() {
+import net.kyori.adventure.text.Component
+import org.bukkit.Bukkit
+import org.bukkit.World
 
+class FirstWorldEntryHandler(val area: Area) {
+    fun update() {
+        val environment = area.player!!.world.environment
+
+        when (environment) {
+            World.Environment.NORMAL -> onFirstEnterOver()
+            World.Environment.NETHER -> onFirstEnterNether()
+            World.Environment.THE_END -> onFirstEnterEnd()
+            else -> Bukkit.getLogger().warning(
+                "\"${area.player!!.world.name}\" 월드는 지원되지 않는 유형의 월드입니다")
+        }
+    }
+
+    fun onFirstEnterOver() {
+        Bukkit.getServer().sendMessage(Component.text("over"))
+    }
+
+    fun onFirstEnterNether() {
+        Bukkit.getServer().sendMessage(Component.text("nether"))
+    }
+
+    fun onFirstEnterEnd() {
+        Bukkit.getServer().sendMessage(Component.text("end"))
     }
 }
