7b113209fa46dac9dcdd0a16c8b778bd7ebbd241
it <yeongbin0909@naver.com>
2025-05-31 21:32:02 +0900

    직렬화기에 월드 진입이력 속성 추가

--- src/main/kotlin/com/github/ityeri/chunkHah/core/serializer/AreaSerializer.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/core/serializer/AreaSerializer.kt
@@ -14,8 +14,13 @@ object AreaSerializer : KSerializer<Area> {
         buildClassSerialDescriptor("Area") {
             element<String>("playerUUID")
             element<Boolean>("isBind")
+
             element<Int>("x")
             element<Int>("z")
+
+            element<Boolean>("isEnteredOver")
+            element<Boolean>("isEnteredNether")
+            element<Boolean>("isEnteredEnd")
         }
 
     override fun serialize(encoder: Encoder, value: Area) {
@@ -26,16 +31,25 @@ object AreaSerializer : KSerializer<Area> {
         composite.encodeIntElement(descriptor, 2, value.x)
         composite.encodeIntElement(descriptor, 3, value.z)
 
+        composite.encodeBooleanElement(descriptor, 4, value.isEnteredOver)
+        composite.encodeBooleanElement(descriptor, 5, value.isEnteredNether)
+        composite.encodeBooleanElement(descriptor, 6, value.isEnteredEnd)
+
         composite.endStructure(descriptor)
     }
 
     override fun deserialize(decoder: Decoder): Area {
         val composite = decoder.beginStructure(descriptor)
+
         var playerUUID = UUID.randomUUID()
         var enabled = true
 
         var x = 0
         var z = 0
+
+        var isEnteredOver = false
+        var isEnteredNether = false
+        var isEnteredEnd =  false
 
         while (true) {
             when (val index = composite.decodeElementIndex(descriptor)) {
@@ -45,13 +59,20 @@ object AreaSerializer : KSerializer<Area> {
                 2 -> x = composite.decodeIntElement(descriptor, index)
                 3 -> z = composite.decodeIntElement(descriptor, index)
 
+                4 -> isEnteredOver = composite.decodeBooleanElement(descriptor, index)
+                5 -> isEnteredNether = composite.decodeBooleanElement(descriptor, index)
+                6 -> isEnteredEnd = composite.decodeBooleanElement(descriptor, index)
+
                 CompositeDecoder.DECODE_DONE -> break
             }
         }
 
         composite.endStructure(descriptor)
 
-        return Area(playerUUID, x, z, enabled)
+        return Area(
+            playerUUID, null, x, z, enabled,
+            isEnteredOver, isEnteredNether, isEnteredEnd
+        )
     }
 
 }
