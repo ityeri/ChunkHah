d94c3a4895bc8486298dbd59fe596eaecff1fd24
it <yeongbin0909@naver.com>
2025-05-31 22:06:53 +0900

    첫 오버 진입시 나무를 생성하는 코드 추가

--- src/main/kotlin/com/github/ityeri/chunkHah/core/FirstWorldEntryHandler.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/core/FirstWorldEntryHandler.kt
@@ -2,9 +2,35 @@ package com.github.ityeri.chunkHah.core
 
 import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.TreeType
 import org.bukkit.World
 
 class FirstWorldEntryHandler(val area: Area) {
+
+    val treeTypes = setOf(
+        TreeType.TREE,
+        TreeType.BIG_TREE,
+        TreeType.REDWOOD,
+        TreeType.TALL_REDWOOD,
+        TreeType.BIRCH,
+        TreeType.JUNGLE,
+        TreeType.SMALL_JUNGLE,
+        TreeType.COCOA_TREE,
+        TreeType.JUNGLE_BUSH,
+        TreeType.SWAMP,
+        TreeType.ACACIA,
+        TreeType.DARK_OAK,
+        TreeType.MEGA_REDWOOD,
+        TreeType.MEGA_PINE,
+        TreeType.TALL_BIRCH,
+        TreeType.CRIMSON_FUNGUS,
+        TreeType.WARPED_FUNGUS,
+        TreeType.AZALEA,
+        TreeType.MANGROVE,
+        TreeType.CHERRY
+    )
+
     fun update() {
         val environment = area.player!!.world.environment
 
@@ -30,7 +56,17 @@ class FirstWorldEntryHandler(val area: Area) {
     }
 
     fun onFirstEnterOver() {
-        Bukkit.getServer().sendMessage(Component.text("over"))
+
+        val treeX = area.minX + area.width / 2
+        val treeZ = area.minX + area.depth / 2
+        val treeY = area.player!!.world.getHighestBlockYAt(treeX, treeZ) + 1
+
+        area.player!!.world.generateTree(
+            Location(
+                area.player!!.world, treeX.toDouble(), treeY.toDouble(), treeZ.toDouble()
+            ),
+            treeTypes.random()
+        )
     }
 
     fun onFirstEnterNether() {
