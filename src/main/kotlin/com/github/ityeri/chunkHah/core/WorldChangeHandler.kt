e75e47afa62a3b8649ada4204b579436f4c0300d
it <yeongbin0909@naver.com>
2025-06-03 14:36:06 +0900

    오버에서 네더로 이동시, 지옥문이 존재한다면 해당 지옥문으로 이동 시키는 기능 추가

--- src/main/kotlin/com/github/ityeri/chunkHah/core/WorldChangeHandler.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/core/WorldChangeHandler.kt
@@ -1,7 +1,9 @@
 package com.github.ityeri.chunkHah.core
 
+import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
 import org.bukkit.Location
+import org.bukkit.Material
 import org.bukkit.World
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -9,17 +11,96 @@ import org.bukkit.event.player.PlayerChangedWorldEvent
 import org.bukkit.event.world.PortalCreateEvent
 import org.bukkit.event.world.PortalCreateEvent.CreateReason
 import kotlin.random.Random
+import kotlin.random.nextInt
 
 class WorldChangeHandler(val area: Area) : Listener {
 
+    fun runTaskNextTick(block: () -> Unit) {
+        Bukkit.getScheduler().runTaskLater(
+            area.areaManager!!.plugin,
+            Runnable { block() },
+            1L
+        )
+    }
+
     fun onOverToNether() {
-        TODO()
+
+        val xCheckingOrder = (area.minX until area.maxX).shuffled()
+        val yCheckingOrder = (0 until 256).shuffled()
+        val zCheckingOrder = (area.minZ until area.maxZ).shuffled()
+
+        for (spawnBlockZ in zCheckingOrder) {
+            for (spawnBlockY in yCheckingOrder) {
+                for (spawnBlockX in xCheckingOrder) {
+
+                    val lowerBlock = area.player!!.world.getBlockAt(spawnBlockX, spawnBlockY, spawnBlockZ)
+                    val upperBlock = area.player!!.world.getBlockAt(spawnBlockX, spawnBlockY + 1, spawnBlockZ)
+
+                    if (
+                        lowerBlock.type == Material.NETHER_PORTAL &&
+                        upperBlock.type == Material.NETHER_PORTAL
+                    ) {
+
+                        runTaskNextTick {
+                            area.player!!.teleport(
+                                Location(
+                                    area.player!!.world,
+                                    spawnBlockX + 0.5,
+                                    spawnBlockY.toDouble(),
+                                    spawnBlockZ + 0.5,
+                                )
+                            )
+                        }
+                        return
+                    }
+
+                }
+            }
+        }
+
+        while (true) {
+
+            val spawnBlockX = Random.nextInt(area.minX, area.maxX)
+            val spawnBlockY = Random.nextInt(0, 128)
+            val spawnBlockZ = Random.nextInt(area.minZ, area.maxZ)
+
+            val groundBlock = area.player!!.world.getBlockAt(
+                spawnBlockX, spawnBlockY - 1, spawnBlockZ)
+
+            val lowerBlock = area.player!!.world.getBlockAt(
+                spawnBlockX, spawnBlockY, spawnBlockZ)
+
+            val upperBlock = area.player!!.world.getBlockAt(
+                spawnBlockX, spawnBlockY + 1, spawnBlockZ)
+
+            if (groundBlock.isSolid &&
+                lowerBlock.isEmpty && upperBlock.isEmpty) {
+
+                runTaskNextTick {
+                    area.player!!.teleport(
+                        Location(
+                            area.player!!.world,
+                            spawnBlockX + 0.5,
+                            spawnBlockY + 0.5,
+                            spawnBlockZ + 0.5
+                        )
+                    )
+                }
+                return
+            }
+
+        }
+
     }
 
     @EventHandler
     fun onPlayerChangeWorld(event: PlayerChangedWorldEvent) {
 
+        Bukkit.getLogger().info("체크 전")
+
         if (event.player != area.player) { return }
+
+        Bukkit.getLogger().info("체크 후")
 
         val fromWorld = event.from
         val toWorld = area.player!!.world
@@ -43,16 +124,16 @@ class WorldChangeHandler(val area: Area) : Listener {
                 val spawnBlockY = groundBlock.y + 1
 
                 if (groundBlock.isSolid) {
-                    Bukkit.getScheduler().runTaskLater(area.areaManager!!.plugin, Runnable {
+                    runTaskNextTick {
                         area.player!!.teleport(
                             Location(
-                            toWorld,
-                            spawnBlockX + 0.5,
-                            spawnBlockY + 0.5,
-                            spawnBlockZ + 0.5
+                                toWorld,
+                                spawnBlockX + 0.5,
+                                spawnBlockY + 0.5,
+                                spawnBlockZ + 0.5
+                            )
                         )
-                        )
-                    }, 1L)
+                    }
 
                     break
                 }
