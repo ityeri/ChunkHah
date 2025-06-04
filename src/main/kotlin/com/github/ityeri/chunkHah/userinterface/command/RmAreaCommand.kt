d90b09fe2f65efb54e37c2180dca381c1bf5712c
it <yeongbin0909@naver.com>
2025-06-04 10:13:37 +0900

    타 플레이어의 영역을 제거하는 기능 추가

--- src/main/kotlin/com/github/ityeri/chunkHah/userinterface/command/RmAreaCommand.kt
+++ src/main/kotlin/com/github/ityeri/chunkHah/userinterface/command/RmAreaCommand.kt
@@ -6,6 +6,7 @@ import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Default
 import com.github.ityeri.chunkHah.core.Area
 import com.github.ityeri.chunkHah.core.AreaManager
+import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
@@ -27,6 +28,28 @@ class RmAreaCommand(val areaManager: AreaManager) : BaseCommand() {
 
         if (areaManager.rmArea(sender) is Area) {
             sender.sendMessage("플레이어 \"${sender.name}\" 의 영역이 성공적으로 제거되었습니다")
+        } else {
+            sender.sendMessage("해당 플레이어에게 할당된 영역이 없습니다")
+        }
+    }
+
+    @Default
+    @CommandPermission("@players")
+    fun onCommand(sender: CommandSender, targetPlayerName: String) {
+        if (sender !is Player) {
+            sender.sendMessage("이 명령어는 플레이어만 사용 가능합니다")
+            return
+        }
+
+        val player = Bukkit.getPlayer(targetPlayerName)
+
+        player ?: run {
+            sender.sendMessage("해당 플레이어는 존재하지 않습니다")
+            return
+        }
+
+        if (areaManager.rmArea(player) is Area) {
+            sender.sendMessage("플레이어 \"${player.name}\" 의 영역이 성공적으로 제거되었습니다")
         } else {
             sender.sendMessage("해당 플레이어에게 할당된 영역이 없습니다")
         }
